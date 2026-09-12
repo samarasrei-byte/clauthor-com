36ce8337d4beaba1d21245f4d1a3df83ae2ab285
samarasrei-byte <samaras.rei@gmail.com>
2026-09-12 12:57:06 -0300

    codex: Deixar responsivo

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
+import "./App.css";
 import "./i18n";
 import { captureReferralFromURL } from "./lib/referral";
 import { initSentry } from "./lib/sentry";
